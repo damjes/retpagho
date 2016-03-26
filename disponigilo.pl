84fffefe59d769c7083e60cb24297f12d1981c95
matma6 (inĝ. Mikaelo Gabor) <matma6@matma6.net>
2016-03-27 00:44:36 +0100

    Rekrei disponigan skripton

--- disponigilo.pl
+++
@@ -1,6 +0,0 @@
-% UZU SWI-PROLOG >= 7
-% swipl -s disponigilo [-t celo]
-
-:- ['datumbazo/konsili.pl'].
-
-% PORFARI
---
+++ retpagho.pl
@@ -0,0 +1,17 @@
+% UZU SWI-PROLOG >= 7
+% swipl -s retpagho [-t celo]
+
+:- ['datumbazo/konsili.pl'].
+
+rulu :-
+	writeln('Retpaĝo rulas').
+
+kompilu :-
+	write('Kompili programon...'),
+	qsave_program(retpagho, [
+		goal(rulu),
+		stand_alone(true)]),
+	writeln(' OK!').
+
+disponigu :-
+	restauru_db('datumbazo.json').
